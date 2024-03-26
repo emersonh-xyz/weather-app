2880b6187a37eccb2e91efd5365690356af0daa2
Giovanni A <32625900+gabbate102@users.noreply.github.com>
2024-03-26 14:20:19 -0400

    Update grid layout and add cards to details page

--- src/app/details-page/page.tsx
+++ src/app/details-page/page.tsx
@@ -4,12 +4,12 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 export default function Details() {
   return (
     <div className="">
-      <div className="grid grid-cols-3 grid-rows-2 pt-4 px-8">
+      <div className="grid grid-cols-3 grid-rows-2 pt-4 px-8 pb-2 gap-4">
         <div className="row-span-2">
           <WeatherCard location="London" temperature={20} rainChance={30} />
         </div>
         <div className="col-span-2">
-          <Card>
+          <Card className="h-full p-2.5">
             <p>Hourly Forecast</p>
             <div className="grid grid-cols-12 overflow-x-auto">
               <div></div>
@@ -17,7 +17,7 @@ export default function Details() {
           </Card>
         </div>
         <div className="col-span-2">
-          <Card>
+          <Card className="h-full p-2.5">
             <p>Week Forecast</p>
             <div className="grid grid-cols-12">
               <div><p>10 a.m.</p></div>
@@ -36,8 +36,37 @@ export default function Details() {
           </Card>
         </div>
       </div>
-      <div className="bg-[#5BB8EB] w-full h-screen items-center flex">
-
+      <div className="bg-[#5BB8EB] w-full h-screen grid grid-cols-6 grid-rows-2 px-8 pt-2 gap-4">
+        <Card className="col-span-2 p-2.5">
+          <p>Hi</p>
+        </Card>
+        <Card className="p-2.5">
+          <p>hi</p>
+        </Card>
+        <Card className="p-2.5">
+          <p>hi</p>
+        </Card>
+        <Card className="p-2.5">
+          <p>hi</p>
+        </Card>
+        <Card className="p-2.5">
+          <p>hi</p>
+        </Card>
+        <Card className="col-span-2">
+          <p>Hi</p>
+        </Card>
+        <Card className="p-2.5">
+          <p>hi</p>
+        </Card>
+        <Card className="p-2.5">
+          <p>hi</p>
+        </Card>
+        <Card className="p-2.5">
+          <p>hi</p>
+        </Card>
+        <Card className="p-2.5">
+          <p>hi</p>
+        </Card>
       </div>
     </div>
   )
